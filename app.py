d8aa306dd7f5c93d8f82635f95d18b424c316787
jinho <jinho021712@gmail.com>
2020-04-22 15:34:16 +0900

    로그인 기능 구현

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 #Flask-PyMongo==2.3.0
 from flask_bcrypt import Bcrypt
@@ -8,7 +8,7 @@ from wtform import *
 
 app = Flask(__name__)
 app.secret_key = 'mysecret'
-app.config['MONGO_URI'] = ""
+app.config['MONGO_URI'] = "mongodb+srv://jinho0217:test@cluster0-klwld.gcp.mongodb.net/Flask_Chat_Room?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 bcrypt = Bcrypt(app)
 
@@ -29,9 +29,32 @@ def index():
         hashpass = bcrypt.generate_password_hash(password) #패스워드 암호화
         db_data = {'username':username, 'password':hashpass}
         myuser.insert_one(db_data)
-        return "추가완료"
 
+        #회원가입 성공시 로그인 창으로
+        return redirect(url_for('login'))
+        
     return render_template("index.html", form=reg_form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        password = login_form.password.data
+        msg = '등록되지 않은 '
+        #등록 아이디 체크
+        existing_user = myuser.find_one({'username' : username})
+        if existing_user:
+            pw_check = bcrypt.check_password_hash(existing_user['password'], password)    
+            if pw_check:
+                return '로그인 성공'
+            else:
+                msg += '패스워드 입니다.'
+                return msg
+        else:
+            msg += '아이디 입니다.'
+            return msg
+    return render_template("login.html", form=login_form)
 
 
 if __name__ == "__main__":
--- wtform.py
+++ wtform.py
@@ -1,20 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
 from wtforms.validators import InputRequired, Length, EqualTo
 #wtform 에서 제공하는 폼을 사용.
 #유효성 검사 제공.
-
+   
 
 class RegistForm(FlaskForm):
     #회원가입
 
     username = StringField('username_label', 
-            validators=[InputRequired(message="사용 할 수 없는 아이디입니다."),
+            validators=[InputRequired(message="아이디를 입력하세요."),
             Length(min=4, max=25, message="아이디 길이는 4 ~ 25자로 제한됩니다.")])
     password = PasswordField('password_label',
-            validators=[InputRequired(message="사용 할 수 없는 패스워드입니다."),
+            validators=[InputRequired(message="패스워드를 입력하세요."),
             Length(min=4, max=25, message="패스워드 길이는 4 ~ 25자로 제한됩니다.")])
     confirm_pswd = PasswordField('confirm_pswd_label',
             validators=[InputRequired(message="사용 할 수 없는 패스워드입니다."),
             EqualTo('password', message="패스워드가 일치하지 않습니다.")])
     submit_button = SubmitField('회원가입')
+
+class LoginForm(FlaskForm):
+    #로그인
+    username = StringField('username_label', 
+            validators=[InputRequired(message="아이디를 입력하세요.")])
+    password = PasswordField('password_label', 
+            validators=[InputRequired(message="패스워드를 입력하세요.")])
+    submit_button = SubmitField('로그인')
